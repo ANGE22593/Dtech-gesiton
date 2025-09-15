bda20562a8ba27ce9871bd2b5a44d4f09e019348
ANGE22593 <angenguettia615@gmail.com>
2025-09-15 17:13:00 +0000

    Mise à jour Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -177,6 +177,7 @@ export default function Dashboard({ transactions }: DashboardProps) {
       </div>
 
       {/* ---- Modal Login Admin ---- */}
+      
       {showLogin && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
           <div className="bg-white p-6 rounded-lg shadow-md w-96">
